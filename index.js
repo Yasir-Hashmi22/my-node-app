b89486945ad9d6358b8c766e9e0a786460b8a502
Yasir-Hashmi22 <hashmiyasir95455@gmail.com>
2026-03-26 12:02:58 +0530

    trigger ci/cd pipeline

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World! My CI/CD pipeline works! 🚀')
+ res.send('Hello World! CI/CD pipeline is working perfectly! 🎉')
 })
 
 module.exports = app
